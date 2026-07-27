a91e7b6813c6d28c7d4289d3a08a17e857432aa1
mangalji <rajmangal03@gmail.com>
2026-07-27 15:05:37 +0530

    change the urlfield to imagefield for profile picture uploading

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -26,7 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     mobile_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    profile_pic = models.URLField(null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
